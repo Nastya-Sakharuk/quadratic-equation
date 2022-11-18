728f062d0ddd100835216e1fc090e5535c48e27a
Anastasiya Sakharuk <sakharuk333@gmail.com>
2022-11-18 08:42:22 +0300

    Quadratic Equation

--- src/main/java/com/epam/rd/autotasks/QuadraticEquation.java
+++ src/main/java/com/epam/rd/autotasks/QuadraticEquation.java
@@ -11,9 +11,22 @@ public class QuadraticEquation {
         double a = scanner.nextDouble();
         double b = scanner.nextDouble();
         double c = scanner.nextDouble();
+        double D;
+        double x1;
+        double x2;
+        double x;
 
-
-
+        D = Math.pow(b, 2) - 4*a*c;
+        if (D>0) {
+            x1 = (-b + Math.sqrt(D))/(2*a);
+            x2 = (-b - Math.sqrt(D))/(2*a);
+            System.out.println(x1 + " " + x2);
+        } else if (D==0) {
+            x = (-b + Math.sqrt(D))/(2*a);
+            System.out.println(x);
+        } else {
+            System.out.println("no roots");
+        }
     }
 
 }
